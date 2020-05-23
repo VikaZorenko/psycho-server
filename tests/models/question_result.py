e3f7ea61c02b152d48eee58a02c4099ec585e7b5
VikaZorenko <zorenkovika@gmail.com>
2020-05-23 14:49:13 +0300

    removed all unique constraints

--- tests/models/question_result.py
+++ tests/models/question_result.py
@@ -3,6 +3,6 @@ from utils.models.base_model import BaseModel
 
 
 class QuestionResult(BaseModel):
-    question = models.ForeignKey('tests.Question', on_delete=models.CASCADE, related_name='question_results', unique=True)
-    answer = models.ForeignKey('tests.Answer', on_delete=models.CASCADE, related_name='question_results', unique=True)
+    question = models.ForeignKey('tests.Question', on_delete=models.CASCADE, related_name='question_results')
+    answer = models.ForeignKey('tests.Answer', on_delete=models.CASCADE, related_name='question_results')
     test_result = models.ForeignKey('tests.TestResult', on_delete=models.CASCADE, related_name='question_results')
--- tests/models/test_stat_result.py
+++ tests/models/test_stat_result.py
@@ -4,5 +4,5 @@ from utils.models.base_model import BaseModel
 
 class TestStatResult(BaseModel):
     test_result = models.ForeignKey('tests.TestResult', on_delete=models.CASCADE, related_name='test_stat_results')
-    test_stat = models.ForeignKey('tests.TestStat', on_delete=models.CASCADE, related_name='test_stat_results', unique=True)
+    test_stat = models.ForeignKey('tests.TestStat', on_delete=models.CASCADE, related_name='test_stat_results')
     points = models.IntegerField(default=0, blank=True)
